6a90ba653207f5d7098a45ca5cafbb7757b186fd
chisl <info@chisl.io>
2017-12-27 22:24:50 +0100

    Initial commit

---
+++ ADT7410.cpp
@@ -0,0 +1,13 @@
+/*
+ * name:        ADT7410
+ * description: ±0.5°C Accurate, 16-Bit Digital I2C Temperature Sensor
+ * manuf:       Analog Devices
+ * version:     0.1
+ * url:         http://www.analog.com/media/en/technical-documentation/data-sheets/ADT7410.pdf
+ * date:        2017-12-28
+ * author       https://chisl.io/
+ * file:        ADT7410.cpp
+ */
+
+#include "ADT7410.hpp"
+
